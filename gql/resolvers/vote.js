4c5c297b929b2c0df5765e6f3327e3a47d9cc1b8
Caitlin Trussell <caitlintrussell@gmail.com>
2018-04-28 19:13:43 -0500

    deleted comment

--- gql/resolvers/vote.js
+++ gql/resolvers/vote.js
@@ -1,10 +1,3 @@
 module.exports = {
   date: async vote => await vote.getDate(),
 }
-
-
-// module.exports = {
-//   date: async (vote, __, { req, res, models: { Date } }) => {
-//     return await Date.findById(vote.dateId)
-//   },
-// }
